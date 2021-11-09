df8ce369e754980581a3a459ba848735bf032e35
Satwik Singh <satwiksingh@Satwiks-MacBook-Air.local>
2021-11-09 11:58:47 +0530

    added example.txt and update creator page view

--- src/Creator.jsx
+++ src/Creator.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 
 const Creator = () => {
@@ -12,6 +12,13 @@ const Creator = () => {
     const [generated, setGenerated] = useState(false);
 
     const textarea = useRef(null);
+    const div = useRef(null);
+
+    useEffect(() => {
+        window.setInterval(()=>{
+            if(div!=null){div.current.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})}
+        }, 500)
+    }, [filledTags])
 
     return (
         <div>
@@ -37,7 +44,7 @@ const Creator = () => {
             </div>
 
             <div className="form-container">
-                <div style={{margin: "20px 5px"}}>
+                <div ref={div} className="filled-tags-container" style={{margin: "20px 5px"}}>
                     {
                     filledTags===null?
                     null
@@ -61,15 +68,16 @@ const Creator = () => {
                     <select onChange={(e)=>{setTag(e.target.value)}} value={tag} style={{margin: "0 8px 0 8px"}}>
                         <option default value="#">tag</option>
                         <option value="companies worked at">companies worked at</option>
+                        <option value="designation">designation</option>
+                        <option value="total experience">total experience</option>
                         <option value="skill">skill</option>
                         <option value="experience duration">experience duration</option>
-                        <option value="designation">designation</option>
+                        <option value="collage name">collage name</option>
+                        <option value="certification">certification</option>
+                        <option value="wins">wins</option>
                         <option value="name">name</option>
                         <option value="email">email</option>
-                        <option value="graduation year">graduation year</option>
-                        <option value="phone">phone</option>
-                        <option value="address">address</option>
-                        <option value="collage name">collage name</option>
+                        
                     </select>
                     <button onClick={()=>{
                         var temp;
@@ -81,7 +89,7 @@ const Creator = () => {
                         var l = [start, end, tag]
                         temp.push(l);
                         setFilledTags(temp);
-                        setHelper(!helper)
+                        setHelper(!helper);
                     }} style={{margin: "0 8px 0 8px"}}>add</button>
                 </div>
                 <div className="generate-button">
